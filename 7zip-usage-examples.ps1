82a5ffefce1b3cf345b923f660c4a3c5b0b98674
Igor Popov <igor@igorpopov.io>
2014-08-16 09:47:36 +0300

    Add 7-zip usage examples
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ 7zip-usage-examples.ps1
@@ -0,0 +1,29 @@
+param (
+    [string] $SEVENZIP = "C:\Program Files\7-Zip\7z.exe"
+)
+
+$buildDirectory = "build"
+
+function Main() {
+    $scriptPath = Split-Path -Parent $PSCommandPath
+    Push-Location $scriptPath
+
+    Remove-Item $buildDirectory -Recurse -Force -ErrorAction SilentlyContinue
+    New-Item $buildDirectory -ItemType directory
+    Push-Location .\$buildDirectory
+    . $SEVENZIP a archive-with-top-level-directory.zip "..\files"
+    . $SEVENZIP a archive-without-top-level-directory.zip "..\files\*"
+    . $SEVENZIP x archive-with-top-level-directory.zip
+    Pop-Location
+
+    OpenDirectoryIfInIde $buildDirectory
+
+    Pop-Location
+}
+
+function OpenDirectoryIfInIde($directory) {
+    $inIde = "Windows PowerShell ISE Host" -eq $Host.Name
+    if ($inIde) { explorer $directory }
+}
+
+Main
